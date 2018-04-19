4c2159d6c52d5cf130112e811f7f92a66fe98231
InKwonJeong <37890531+InKwonJeong@users.noreply.github.com>
2018-04-19 20:57:34 +0900

    Update s2_insert.js

--- s2_insert.js
+++ s2_insert.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'me',
     password: 'mypassword',
-    database: 'mydb'
+    database: 'myhw'
 })
 connection.connect();
 
